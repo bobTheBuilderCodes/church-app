ea8eb5a094285bf9de574377d438249afcb4dd27
Sam Robert <91603698+bobTheBuilderCodes@users.noreply.github.com>
2024-10-12 16:17:50 +0000

    Update birthday.tsx

--- client/src/components/feature/birthday.tsx
+++ client/src/components/feature/birthday.tsx
@@ -56,13 +56,13 @@ const people = [
   export default function Birthday() {
     return (
       <ul role="list" className="divide-y divide-gray-100">
-        {people.map((person) => (
-          <li key={person.email} className="flex justify-between gap-x-6 py-5">
+        {people?.map((person) => (
+          <li key={person?.email} className="flex justify-between gap-x-6 py-5">
             <div className="flex min-w-0 gap-x-4">
-              <img alt="" src={person.imageUrl} className="h-8 w-8 flex-none rounded-full bg-gray-50" />
+              <img alt="" src={person?.imageUrl} className="h-8 w-8 flex-none rounded-full bg-gray-50" />
               <div className="min-w-0 flex-auto">
-                <p className="text-sm font-semibold leading-4 text-gray-900">{person.name}</p>
-                <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p>
+                <p className="text-sm font-semibold leading-4 text-gray-900">{person?.name}</p>
+                <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person?.email}</p>
               </div>
             </div>
             

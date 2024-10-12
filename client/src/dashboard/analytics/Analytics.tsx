53708e7d6bddacd34728d3b8b5734eee42bacce4
Sam Robert <91603698+bobTheBuilderCodes@users.noreply.github.com>
2024-10-12 16:27:23 +0000

    Dashboard updated

--- client/src/dashboard/analytics/Analytics.tsx
+++ client/src/dashboard/analytics/Analytics.tsx
@@ -1,16 +1,33 @@
-import React from 'react';
-import Layout from '../Layout';
-import Birthday from '../../components/feature/Birthday';
-import Card from '../../components/ui/DashboardCard';
-import { barChartData, chartData, chartOptions, pieChartData } from '../../resources';
-import LineChart from '../../components/feature/LineGraph';
-import BarChart from '../../components/feature/BarChart';
-import PieChart from '../../components/feature/PieChart';
+import React from "react";
+import Layout from "../Layout";
+import Card from "../../components/ui/DashboardCard";
+import {
+  barChartData,
+  chartData,
+  chartOptions,
+  people,
+  pieChartData,
+} from "../../resources";
+import LineChart from "../../components/feature/LineGraph";
+import BarChart from "../../components/feature/BarChart";
+import PieChart from "../../components/feature/PieChart";
 
 // Icon for user
+
 const user = (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth="1.5"
+    stroke="currentColor"
+    className="size-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+    />
   </svg>
 );
 
@@ -41,7 +58,9 @@ const Analytics: React.FC = () => {
             <div className="flex flex-col lg:flex-row lg:space-x-4 gap-4">
               {/* Bar Chart Container */}
               <div className="bg-white shadow-xl shadow-gray-100 rounded-lg p-4 flex-1">
-                <h3 className="text-lg font-semibold mb-4">Ministry Distribution</h3>
+                <h3 className="text-lg font-semibold mb-4">
+                  Ministry Distribution
+                </h3>
                 <div className="w-full h-80">
                   <BarChart data={barChartData} />
                 </div>
@@ -59,14 +78,39 @@ const Analytics: React.FC = () => {
 
           {/* Active Members List */}
           <div className="bg-white shadow-xl shadow-gray-100 rounded-lg p-6 h-fit">
-            <h3 className="text-lg font-semibold mb-4">Birthdays in September</h3>
+            <h3 className="text-lg font-semibold mb-4">
+              Birthdays in September
+            </h3>
             <hr />
-            <Birthday />
+            <ul role="list" className="divide-y divide-gray-100">
+              {people?.map((person) => (
+                <li
+                  key={person?.email}
+                  className="flex justify-between gap-x-6 py-5"
+                >
+                  <div className="flex min-w-0 gap-x-4">
+                    <img
+                      alt=""
+                      src={person?.imageUrl}
+                      className="h-8 w-8 flex-none rounded-full bg-gray-50"
+                    />
+                    <div className="min-w-0 flex-auto">
+                      <p className="text-sm font-semibold leading-4 text-gray-900">
+                        {person?.name}
+                      </p>
+                      <p className="mt-1 truncate text-xs leading-5 text-gray-500">
+                        {person?.email}
+                      </p>
+                    </div>
+                  </div>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
     </Layout>
   );
-}
+};
 
 export default Analytics;
--- client/src/resources/index.ts
+++ client/src/resources/index.ts
@@ -471,3 +471,58 @@ export const churchEvents = [
     imageUrl: "https://images.unsplash.com/photo-1531944252668-83d381a30b26?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8N3x8Q2h1cmNoJTIwcGljdHVyZXN8ZW58MHx8MHx8fDA%3D", // Christmas Eve Image
   },
 ];
+
+export const people = [
+  {
+    name: 'Leslie Alexander',
+    email: 'leslie.alexander@example.com',
+    role: 'Co-Founder / CEO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Michael Foster',
+    email: 'michael.foster@example.com',
+    role: 'Co-Founder / CTO',
+    imageUrl:
+      'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Dries Vincent',
+    email: 'dries.vincent@example.com',
+    role: 'Business Relations',
+    imageUrl:
+      'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: null,
+  },
+  {
+    name: 'Lindsay Walton',
+    email: 'lindsay.walton@example.com',
+    role: 'Front-end Developer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Courtney Henry',
+    email: 'courtney.henry@example.com',
+    role: 'Designer',
+    imageUrl:
+      'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: '3h ago',
+    lastSeenDateTime: '2023-01-23T13:23Z',
+  },
+  {
+    name: 'Tom Cook',
+    email: 'tom.cook@example.com',
+    role: 'Director of Product',
+    imageUrl:
+      'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+    lastSeen: null,
+  },
+]
